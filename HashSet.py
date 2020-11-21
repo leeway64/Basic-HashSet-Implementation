7cad72f4d6421a79ce614661533b791a6c79a85a
Lee-Way Wang <lee.way56@comcast.net>
2020-11-21 11:41:38 -0800

    added contains, size, and indexOf functions to the object

--- HashSet.py
+++ HashSet.py
@@ -1,6 +1,28 @@
 class HashSet:
-    def __init__(self, capacity=None):
-        self.data = HashNode
+    def __init__(self, capacity):
+        self.__data = []
+        self.__hash_table_length = capacity / 0.75
+        self.__size = 0
+
+
+    def contains(self, value):
+        i = self.__index_of(value)
+        current = self.__data[i]
+        while current is not None:
+            if current.data == value:
+                return True
+            current = current.next
+        return False
+
+
+    def __size(self):
+        return self.__size
+
+    def __index_of(self, value):
+        return abs(hash(value) % self.__hash_table_length)
+
 
     class HashNode:
-        
+        def __init__(self, value=None, next=None):
+            self.value = value
+            self.next = next
