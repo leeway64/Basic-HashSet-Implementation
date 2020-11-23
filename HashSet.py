6e196e4d771988cf2aca6cc341742e3295cb33e2
Lee-Way Wang <lee.way56@comcast.net>
2020-11-23 15:28:40 -0800

    Fixed several bugs. HashSet.py should now have the same behavior as HashSet.Java

--- HashSet.py
+++ HashSet.py
@@ -1,24 +1,27 @@
-# 11/21/2020
+# 11/22/2020
 class HashSet:
     def __init__(self, capacity):
         self.__data = []
         self.__hash_table_length = capacity / 0.75  # Load factor: 0.75
+        # print(self.__hash_table_length)
+        # print(capacity)
+        self.__initialize_data(int(self.__hash_table_length))
         self.__size = 0
 
     def add(self, value):
         if not self.contains(value):
             i = self.__index_of(value)
-            self.data[i] = self.HashNode(value, self.data[i])
+            self.__data[i] = self.__HashNode(value, self.__data[i])
             self.__size += 1
 
     def remove(self, value):
         if self.contains(value):
-            i = self.__indexOf(value)
-            if self.__data[i] == value:
+            i = self.__index_of(value)
+            if self.__data[i].value == value:
                 self.__data[i] = self.__data[i].next
             else:
                 current = self.__data[i]
-                while current.next.data != value:
+                while current.next.value != value:
                     current = current.next
                 current.next = current.next.next
             self.__size -= 1
@@ -27,7 +30,7 @@ class HashSet:
         i = self.__index_of(value)
         current = self.__data[i]
         while current is not None:
-            if current.data == value:
+            if current.value == value:
                 return True
             current = current.next
         return False
@@ -36,9 +39,13 @@ class HashSet:
         return self.__size
 
     def __index_of(self, value):
-        return abs(hash(value) % self.__hash_table_length)
+        return int(abs(hash(value) % self.__hash_table_length))
 
-    class HashNode:
+    def __initialize_data(self, size):
+        for i in range(size):
+            self.__data.append(self.__HashNode())
+
+    class __HashNode:
         def __init__(self, value=None, next=None):
             self.value = value
             self.next = next
