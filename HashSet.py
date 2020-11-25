ebf38ade11f2a665c388d39cded12f975b7b5b11
Lee-Way Wang <lee.way56@comcast.net>
2020-11-25 13:13:45 -0800

    added __str__ function

--- HashSet.py
+++ HashSet.py
@@ -2,15 +2,15 @@
 class HashSet:
     def __init__(self, capacity):
         self.__data = []
-        self.__hash_table_length = capacity / 0.75  # Load factor: 0.75
+        self.__hash_table_length = int(capacity / 0.75)  # Load factor: 0.75
         # print(self.__hash_table_length)
         # print(capacity)
-        self.__initialize_data(int(self.__hash_table_length))
+        self.__initialize_data(self.__hash_table_length)
         self.__size = 0
 
     def add(self, *values):
         for value in values:
-            if not self.contains(value):
+            if not self.contains(value):  # Does not allow any duplicates
                 i = self.__index_of(value)
                 self.__data[i] = self.__HashNode(value, self.__data[i])
                 self.__size += 1
@@ -40,11 +40,23 @@ class HashSet:
         return self.__size
 
     def __index_of(self, value):
-        return int(abs(hash(value) % self.__hash_table_length))
+        # print(int(abs(hash(value) % self.__hash_table_length)))
+        return int(abs(hash(value) % self.__hash_table_length))  # Hash code is the same for ints
 
-    def __initialize_data(self, size):
-        for i in range(size):
+    def __initialize_data(self, table_length):
+        for i in range(table_length):
             self.__data.append(self.__HashNode())
+
+    def __str__(self):
+        result = "{\n"
+        for i in range(self.__hash_table_length):
+            if self.__data[i].value is not None:
+                current = self.__data[i]
+                while current.value is not None:
+                    result += str(i) + ': ' + str(current.value) + '\n'
+                    current = current.next
+        result += "}"
+        return result
 
     class __HashNode:
         def __init__(self, value=None, next=None):
